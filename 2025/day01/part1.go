23ce7c2892f24e2a2f109007dfb4d84153bdcfc6
Tomygood <66916111+Tomygood@users.noreply.github.com>
2025-12-01 06:18:47 +0100

    2025 day 1!

--- 2025/day01/part1.go
+++ 2025/day01/part1.go
@@ -10,8 +10,22 @@ import (
 func part1() {
 
 	lines := utils.Lines(inputDay)
-
 	res := 0
+	dial := 50
+	for _, line := range lines {
+
+		n := utils.Atoi(line[1:])
+		if line[0] == 'R' {
+			dial = utils.Mod(dial+n, 100)
+		} else {
+			dial = utils.Mod(dial-n, 100)
+
+		}
+
+		if dial == 0 {
+			res++
+		}
+	}
 
 	fmt.Println(res)
 	utils.ToClipboard(res)
--- 2025/day01/part2.go
+++ 2025/day01/part2.go
@@ -2,8 +2,37 @@ package main
 
 import (
 	_ "embed"
+	"fmt"
+
+	"github.com/Tomygood/advent-of-code/utils"
 )
 
 func part2() {
 
+	lines := utils.Lines(inputDay)
+	res := 0
+	dial := 50
+	for _, line := range lines {
+
+		n := utils.Atoi(line[1:])
+		if line[0] == 'R' {
+			for range n {
+				dial = utils.Mod(dial+1, 100)
+				if dial == 0 {
+					res++
+				}
+			}
+
+		} else {
+			for range n {
+				dial = utils.Mod(dial-1, 100)
+				if dial == 0 {
+					res++
+				}
+			}
+		}
+	}
+
+	fmt.Println(res)
+	utils.ToClipboard(res)
 }
